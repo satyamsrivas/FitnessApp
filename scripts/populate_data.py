f6c444c2cbbef7f498195424bf26ae8d1527b30a
Satyam Srivastava <satyams876@gmail.com>
2025-02-14 13:55:11 +0000

    update populate data script

--- scripts/populate_data.py
+++ scripts/populate_data.py
@@ -1,8 +1,27 @@
-from users.models import User
-from django.db.models.functions import Upper
-from django.db import connection
+from users.models import User,FitnessProfile
+from faker import Faker
+import random
+
+
+fake = Faker()
 
 def run():
-    user = User.objects.filter().values_list('first_name',flat=True)
-    print(user)
-    print(connection.queries)
+    users = []
+    for _ in range(20):
+        user = User.objects.create(
+            first_name=fake.first_name(),
+            last_name=fake.last_name() if random.choice([True, False]) else "",
+            mobile_number=fake.unique.numerify("98########"),  # Generates a 10-digit number
+            email=fake.email(),
+            is_staff=random.choice([True, False]),
+        )
+        users.append(user)
+
+        for user in users:
+            FitnessProfile.objects.create(
+                user=user,
+                height=round(random.uniform(150, 200), 2),  # Height in cm
+                weight=round(random.uniform(50, 100), 2),  # Weight in kg
+                age=random.randint(18, 60),
+                gender=random.choice(["M", "F"]),
+            )
